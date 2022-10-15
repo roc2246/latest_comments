93449bdcaa7226d6053e82ae3d4f85024165680c
Riley <riley.childs@yahoo.com>
2022-10-15 10:17:59 -0400

    Updated COmment on user router

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 const User = require('../models/user')
 
-// Retrieves comments
+// Retrieves User Credentials
 router.get('/',  (req, res) => {
      User.find().then((result) => {
       res.send(result)
